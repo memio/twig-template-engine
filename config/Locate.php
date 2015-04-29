7d8c215648287eb44be290ceaa06b0d2d0a878f9
Loïc Chardonnet <loic.chardonnet@gmail.com>
2015-04-29 18:55:00 +0100

    Fixed Build namespace

--- config/Locate.php
+++ config/Locate.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace Memio\PrettyPrinter\Config;
+/*
+ * This file is part of the memio/twig-template-engine package.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Memio\TwigTemplateEngine\Config;
 
 class Locate
 {
